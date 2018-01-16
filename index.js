65aebbef149d9db770e9cd38cb40720fe71428d4
Michael Kirschbaum <hi@michaelkirschbaum.com>
2018-01-16 14:49:39 -0800

    setup Docker

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import Express from 'express';
 
 const app = new Express();
 
-app.post('/sms', (req, res) => {
-  console.log('message received');
+  app.post('/sms', (req, res) => {
+    console.log("message received");
 })
 
 http.createServer(app).listen(3000, () => console.log("listening on port 3000"));
