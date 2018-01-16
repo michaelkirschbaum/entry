964dab70d58a963fbfc8489ac68d9dcb5c203a7c
Michael Kirschbaum <hi@michaelkirschbaum.com>
2018-01-16 09:20:16 -0800

    add '/' route

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ import Twilio from 'twilio';
 
 const app = new Express();
 
+app.get('/', function(req, res) {
+  res.send('keyless');
+})
+
 app.listen(3000, () => console.log("listening on port 3000"));
